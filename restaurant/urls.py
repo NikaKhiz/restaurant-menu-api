a59787d4b4a075b90c3257189abf96b3b53e53ed
NikaKhiz <khizambarelinika123@gmail.com>
2024-11-14 14:36:24 +0400

    default router register userviewset

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,21 +3,14 @@ from django.urls import path, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 from debug_toolbar.toolbar import debug_toolbar_urls
+from user.views import UserViewSet
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest-framework'))
 ] + debug_toolbar_urls()
