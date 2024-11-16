1b3864a71a1a682085ac3d46f365505508a6cbb0
NikaKhiz <khizambarelinika123@gmail.com>
2024-11-16 15:16:12 +0400

    remove unnecessary actions optimize queries

--- lunchroom/views.py
+++ lunchroom/views.py
@@ -1,8 +1,6 @@
 from rest_framework import  viewsets, permissions
 from .models import Restaurant, Menu, Dish
 from .serializers import RestaurantSerializer, MenuSerializer, SubMenuSerializer, DishSerializer
-from rest_framework.decorators import action
-from rest_framework.response import Response
 
 class RestaurantViewSet(viewsets.ModelViewSet):
     queryset = Restaurant.objects.all()
@@ -17,20 +15,14 @@ class MenuViewSet(viewsets.ModelViewSet):
 
 
 class SubMenuViewSet(viewsets.ModelViewSet):
-    queryset = Menu.objects.filter(parent__isnull=False).prefetch_related('dishes')
+    queryset = Menu.objects.filter(parent__isnull=False).prefetch_related('dishes__ingredients')
     serializer_class = SubMenuSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     filterset_fields = ('parent', 'dishes')
 
 
-    @action(detail=True, methods=['get'])
-    def get_dishes(self, request, pk=None):
-        submenu = Menu.objects.filter(id=pk).first()
-        dishes = submenu.dishes.all() 
-        return Response(DishSerializer(dishes, many=True).data)
-    
 class DishViewSet(viewsets.ModelViewSet):
-    queryset = Dish.objects.all()
+    queryset = Dish.objects.prefetch_related('ingredients')
     serializer_class = DishSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     filterset_fields = ('name',)
